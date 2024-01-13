466ab8af6d310fd02f92eda376ce7559b4695118
Mishab <mishabmsb91@gmail.com>
2024-01-13 14:19:07 +0530

    first commit

---
+++ config/collections.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PRODUCTS_COLLECTION: 'products',
+    USERS_COLLECTION: 'users',
+    ADMIN_COLLECTION: 'admin',
+    CART_COLLECTION: 'cart',
+    ORDER_COLLECTION: 'order',
+    PROJECT_COLLECTION: "project",
+
+}
---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mongoClient = require("mongodb").MongoClient;
+
+const state = {
+  db: null,
+};
+
+module.exports.connect = function (done) {
+  // const url = "mongodb+srv://thevectorcrop:msb.com001@vc-projects.jdlpp5l.mongodb.net/?retryWrites=true&w=majority";
+  const url = "mongodb+srv://thevectorcrop:msb.com002@mycms.dvf8n7l.mongodb.net/?retryWrites=true&w=majority";
+
+  const dbname = "vc-projects";
+
+  mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    state.db = data.db(dbname);
+
+    done();
+  });
+};
+
+module.exports.get = function () {
+  return state.db;
+};
